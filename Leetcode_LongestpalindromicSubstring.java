925517a7c44f461f35806db8f44fde8342b6b87a
sureshkumarreddy-k <112793655+sureshkumarreddy-k@users.noreply.github.com>
2023-07-23 20:32:08 +0530

    Create Leetcode_LongestpalindromicSubstring.java

---
+++ Leetcode_LongestpalindromicSubstring.java
@@ -0,0 +1,29 @@
+// Question 7: Longest Palindromic Substring
+
+class Solution {
+    public String longestPalindrome(String s) {
+       int start = 0, end = 0;
+       for(int i = 0; i < s.length(); i ++) {
+           char ch = s.charAt(i);
+           int left = i;
+           int right = i;
+           while(left >= 0 && s.charAt(left) == ch){
+               left --;
+           }
+           while(right < s.length() && s.charAt(right) == ch){ 
+               right ++;
+           }
+           while(left >= 0 && right < s.length()) {
+               if(s.charAt(left) != s.charAt(right)) break;
+               left --;
+               right ++;
+           }
+           left += 1;
+           if(end - start < right - left) {
+               start = left;
+               end = right;
+           }
+       } 
+       return s.substring(start, end);
+    }
+}
