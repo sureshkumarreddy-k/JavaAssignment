84d17302f2de99c3507cedd1899a845d38701db6
sureshkumarreddy-k <112793655+sureshkumarreddy-k@users.noreply.github.com>
2023-07-23 20:37:28 +0530

    Create LeetCode_SwapNodesInPairs.java

---
+++ LeetCode_SwapNodesInPairs.java
@@ -0,0 +1,19 @@
+// Question 11: Swap Nodes in Pair
+
+
+class Solution {
+    public ListNode swapPairs(ListNode head) {
+        ListNode temp = new ListNode(0);
+        temp.next = head;
+        ListNode current = temp;
+        while (current.next != null && current.next.next != null) {
+            ListNode first = current.next;
+            ListNode second = current.next.next;
+            first.next = second.next;
+            current.next = second;
+            current.next.next = first;
+            current = current.next.next;
+        }
+        return temp.next;
+    }
+}
