ba704a83ed623836afb0039bec801d14fc6c7170
sureshkumarreddy-k <112793655+sureshkumarreddy-k@users.noreply.github.com>
2023-07-23 20:39:32 +0530

    Update Sub_Array.java

--- Sub_Array.java
+++ Sub_Array.java
@@ -4,10 +4,9 @@
 import java.io.*;
 import java.util.*;
 
-public class Sub_Array {
+public class Solution {
 
     public static void main(String[] args) {
-        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
         Scanner scan=new Scanner(System.in);
         int n= scan.nextInt();
         int arr[]=new int[n];
